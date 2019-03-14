347b76a005687fd63dc190bfc7fa6fc544f4d704
avakyants <tema14@gmail.com>
2019-03-14 15:27:35 +0300

    Task C completed

--- src/TaskC.java
+++ src/TaskC.java
@@ -1,23 +1,34 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskC {
     public static void main(String[] args) throws Exception {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        AtomicInteger rows = new AtomicInteger(read(r));
 
-        int rows = Integer.parseInt(r.readLine());
 
-        int lastN = 0 ;
-        int cnt = 0;
-        while (rows>0){
-            int n = Integer.parseInt(r.readLine());
+        if(rows.intValue()>0) {
 
-            if(cnt==0 || n>lastN) System.out.println(n);
-            lastN=n;
-            rows--;
-            cnt++;
+            int lastN = read(r);
+            System.out.println(lastN);
+
+            while (rows.decrementAndGet() > 0) {
+                int n = read(r);
+                if (n > lastN) System.out.println(n);
+                lastN = n;
+                if(rows.get()%100000==0)System.gc();
+            }
+
         }
 
         r.close();
+
     }
+
+    public static int read(BufferedReader r) throws Exception{
+        return Integer.parseInt(r.readLine());
+    }
+
+
 }
